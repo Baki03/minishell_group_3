986e9623529c6ee8e016e8819851effb269e1114
Baki03 <138146680+Baki03@users.noreply.github.com>
2024-07-19 17:56:28 +0200

    init

--- parsing/is/is_word.c
+++ parsing/is/is_word.c
@@ -6,9 +6,25 @@
 /*   By: pepi <pepi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:42:42 by pepi              #+#    #+#             */
-/*   Updated: 2024/07/19 17:43:18 by pepi             ###   ########.fr       */
+/*   Updated: 2024/07/19 17:56:17 by pepi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+int is_word(char    *content)
+{
+    int i;
+
+    i = 0;
+    if ((is_in_quotes(content)) || (is_file(content))
+        || (is_dollar_word(content)) || (is_flag(content)))
+        return (0);
+    while (content[i])
+    {
+        if (is_redir(content, i))
+            return (0);
+        i++;
+    }
+    return (1);
+}
